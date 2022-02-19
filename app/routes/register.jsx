ba8f1c5a2f7a3c38bb70b0c858585984f9d8632f
David Barry <david.barry@ipg-automotive.com>
2022-02-19 11:35:21 +0000

    add extra routes as demo

---
+++ app/routes/register.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Register page goes here
+      </Typography>
+    </React.Fragment>
+  );
+}
